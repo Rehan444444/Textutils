856075147d67a11debbe006b7506ef4afc07719c
Sandeep <sandeep836815@gmail.com>
2023-03-06 04:08:28 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+import Textform from './components/Textform'
+// import { useState } from 'react';
+import React,{useInsertionEffect, useState} from 'react';
+
+import Layout from './components/Layout'
+import Alert from './components/Alert'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-Router-dom";
+
+
+function App() {
+  const[Mode, setMode]=useState('light');
+  // const[alert,setAlert]=useState(null);
+
+
+  // const showAlert=(message,type)=>{
+  //      setAlert({
+  //       msg:message,
+  //       type:type
+  //      })
+  // }
+
+
+
+  const toggleMode=()=>{
+    if(Mode=== 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='rgb(26 38 80)';
+      // showAlert('Dark mode has been Enable', 'Success');
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      // showAlert('light mode has been Enable', 'Success');
+    }
+  }
+
+  return (
+   <> 
+  <Navbar title="Textutils" aboutText="about us" mode={Mode} toggleMode={toggleMode}/>
+  {/* <Alert alert={alert}/> */}
+  
+  {/* <Router> */}
+  <div className="container">
+  {/* <Switch>
+          <Route exact path="/Layout">
+            <Layout />
+          </Route> */}
+         
+          {/* <Route exact path="/" component={Textform}> */}
+          <Textform heading="Enter the text for analyse" mode={Mode}/>
+          {/* <Navbar/> */}
+          {/* </Route>
+          </Switch>
+               */}
+  </div>
+  {/* </Router> */}
+ 
+ {/* <div className="loading">
+  <Layout/>
+ </div> */}
+ 
+   
+   </>
+  );
+}
+
+export default App;
+
---
+++ src/components/Textform.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+export default function Textform(props) {
+  const handleupclick = () => {
+    // console.log("Uppercase was clicked"+ text);
+    let newText = text.toUpperCase();
+
+    setText(newText)
+
+  }
+  const handlelowclick = () => {
+    // console.log("Uppercase was clicked"+ text);
+    let newText = text.toLowerCase();
+
+    setText(newText)
+
+  }
+  const handleclearclick = () => {
+    // console.log("Uppercase was clicked"+ text);
+    let newText = ' ';
+
+    setText(newText)
+
+  }
+  const handleuplowclick = () => {
+    // console.log("Uppercase was clicked"+ text);
+    let newText = text.charAt(0).toUpperCase()
+      + text.slice(1);
+
+    setText(newText)
+
+  }
+
+
+
+  const handleonchange = (event) => {
+    // console.log("on change working");
+
+    setText(event.target.value)
+  }
+
+
+  const [text, setText] = useState('Enter the text here');
+  return (
+    <>
+
+      {/* <div className="mb-3" style={{color:props.mode==='dark'?'white':'black'}}>
+  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" style={{backgroundColor:props.mode==='dark'?'#4e3f68':'white',color:props.mode==='dark'?'white':'black'}}  id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div> */}
+      <div className="container2"  style={{ color: props.mode === 'dark' ? 'white' : 'black' }} >
+        <h1>{props.heading}</h1>
+      
+      <div className="mb-3">
+        {/* <label for="exampleFormControlTextarea1" className="form-label">{props.heading}</label> */}
+        <textarea className="form-control" onChange={handleonchange} style={{ backgroundColor: props.mode === 'dark' ? 'rgb(26 38 80)' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} value={text} id="exampleFormControlTextarea1" rows="12" ></textarea>
+
+
+        <button className="btn btn-primary my-3" onClick={handleupclick}>Convert to Uppercase</button>
+        <button className="btn btn-danger mx-3" onClick={handlelowclick} >Convert to lowercase</button>
+        <button className="btn btn-success " onClick={handleclearclick} >Clear Text</button>
+        <button className="btn btn-secondary mx-3" onClick={handleuplowclick} >Convert to Upper+Lower</button>
+
+        </div>
+      </div>
+
+      <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h1>Summary</h1>
+        <p>{text.split(" ").length} Words an {text.length} Characters</p>
+        <p>{0.008 * text.split(" ").length}minutes required to read this paragragh</p>
+
+        <h1>Preview</h1>
+        <p>{text.length>0?text:"Enter to preview"}</p>
+
+      </div>
+    </>
+
+  )
+}
